df511a0300c2c88d3f031d3a4cf288e16027771c
Anthony Bui <anthonybui@gmail.com>
2019-05-03 15:20:24 -0400

    Big commit. Sowweee

--- classification/resizeimg.py
+++ classification/resizeimg.py
@@ -1,9 +1,16 @@
 from PIL import Image
 from os import listdir
 
-files = [f for f in listdir("./hires/noncriminal/")]
+files = [f for f in listdir("./train-fullsize/noncriminal/")]
 
 for file in files:
-    img = Image.open("./hires/noncriminal/"+file)
+    img = Image.open("./train-fullsize/noncriminal/"+file)
     newIMG = img.resize((300,300))
-    newIMG.save(file[0:-4]+"-300.png", "PNG")
+    newIMG.save("train-300/noncriminal/"+file[0:-4]+".png", "PNG")
+
+files = [f for f in listdir("./train-fullsize/criminal/")]
+
+for file in files:
+    img = Image.open("./train-fullsize/criminal/"+file)
+    newIMG = img.resize((300,300))
+    newIMG.save("train-300/criminal/"+file[0:-4]+".png", "PNG")
